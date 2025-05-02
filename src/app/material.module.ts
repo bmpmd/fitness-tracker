3e1accc5189303a3ea036cb9247f062422b7bf78
leo juarez <sk8leo@live.com>
2025-05-02 13:28:19 -0400

    imported ng mat, using module for mat components

---
+++ src/app/material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+
+@NgModule({
+    imports: [],
+    exports: []
+})
+export class MaterialModule{
+    //using module for only material components as suggested 
+    // by the docs instead 
+    
+
+}
